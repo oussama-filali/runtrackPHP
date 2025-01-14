2cf0378998c479d7b414a8cb03b7d89496001ec8
oussama-fiali <oussama.halima-filali@laplateforme.io>
2025-01-14 23:21:57 +0100

    ajout du fichier index02 dans le dossier job02

---
+++ jour01/job02/index2.php
@@ -0,0 +1,7 @@
+<?php
+// Affichage avec un commentaire sur la même ligne.
+echo "Hello LaPlateforme!"; // Ceci est un commentaire en ligne.
+
+/* Affichage avec un commentaire en bloc. */
+echo "Hello LaPlateforme!";
+?>
