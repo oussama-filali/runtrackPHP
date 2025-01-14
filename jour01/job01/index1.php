8e2b856b944f63963d76272f519eb8b9e24c27ec
oussama-fiali <oussama.halima-filali@laplateforme.io>
2025-01-14 23:17:36 +0100

    ajout du nouveau fichier dans job01

---
+++ jour01/job01/index1.php
@@ -0,0 +1,22 @@
+<?php
+// Étape 1 : Créez une variable et affichez-la.
+$str = "LaPlateforme";
+echo $str;
+
+// Étape 2 : Concaténez des variables.
+$str2 = "Vive";
+$str3 = "!";
+echo "\n" . $str2 . " " . $str . " " . $str3;
+
+// Étape 3 : Manipulez un entier.
+$val = 6;
+echo "\n" . $val;
+$val += 4;
+echo "\n" . $val;
+
+// Étape 4 : Manipulez un booléen.
+$myBool = true;
+echo "\n" . $myBool;
+$myBool = false;
+echo "\n" . $myBool;
+?>
