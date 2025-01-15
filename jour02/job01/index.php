1ca6f9a88b3c371a159aa1a5333042fe36596aeb
oussama-fiali <oussama.halima-filali@laplateforme.io>
2025-01-15 14:41:35 +0100

    modification du fichier index

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,7 +1,9 @@
-<?php ;
-for ($number= 0; $number <= 1037 ;$number++)
-{
-    echo "$number <br>";
+<?php
+for ($number = 0; $number <= 1337; $number++) {
+    if ($number == 42) {
+        echo "<b><u>$number</u></b><br>";  // Gras et souligné pour 42.
+    } else {
+        echo $number . "<br>";  // Sinon, affiche le nombre normalement.
+    }
 }
-
 ?>
