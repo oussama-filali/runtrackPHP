759d5b3b6e270da6150bbd8f0939a99242e5845e
oussama-fiali <oussama.halima-filali@laplateforme.io>
2025-01-18 16:07:34 +0100

    soumettre un formulaire de connexion avec identifiant et password avec un resultat prédéfini

---
+++ jour04/job05.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire de Connexion</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" required><br><br>
+        
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required><br><br>
+        
+        <input type="submit" value="Se connecter">
+    </form>
+
+    <?php
+    // Vérifier si le formulaire a été soumis
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // Vérifier les identifiants
+        if ($username === 'John' && $password === 'Rambo') {
+            echo "<p>c’est pas ma guerre</p>";
+        } else {
+            echo "<p>votre pire cauchemar</p>";
+        }
+    }
+    ?>
+</body>
+</html>
