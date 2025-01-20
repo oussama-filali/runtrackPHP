170c2e02263de2940bdbf4c2a33149a23f112706
oussama-fiali <oussama.halima-filali@laplateforme.io>
2025-01-20 14:14:02 +0100

    ajout du job03

---
+++ job03.php
@@ -0,0 +1,9 @@
+<?php
+//Déclaration d'une fonction 
+function getHello(): string {
+    return "Hello LaPlateforme!";
+}
+
+//Appel de la fonction et affichage de sa valeur de retour
+echo getHello();
+?>
