9a373624478db9c2ea778c9809ef72bdb4e52662
oussama-fiali <oussama.halima-filali@laplateforme.io>
2025-01-20 14:11:48 +0100

    modification du code dans le job02

--- jour05/job02.php
+++ jour05/job02.php
@@ -1,12 +1,19 @@
 <?php
+function bonsoir(bool $jour):void{
+if($jour){
+echo"Bonjour";
+}else{
+echo"Bonsoir";}}
 function bonjour (bool $jour): void {
     if ($jour) {
     echo "Bonjour";
+    } else {
+        echo "Bonsoir";}
     }
     
-}
+
 
 bonjour(true);
-bonjour(false);
+bonsoir(false);
 
 ?>
